1521a5c89540227a12de6d7cd8266f2d0ee2445e
BennoDev <bennokohrs@gmail.com>
2020-08-17 10:05:40 +0200

    Changed example for testing purpose

--- examples/react-typescript/src/App.tsx
+++ examples/react-typescript/src/App.tsx
@@ -1,32 +1,27 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {useAgile} from 'agile-framework';
-import {Test} from "./core";
+import {useAgile, AgileHOC} from 'agile-framework';
+import {MY_STATE, MY_STATE_2, Test} from "./core";
 
-function App() {
+const App = (props: any) => {
 
-   // const [myState] = useAgile([MY_STATE]);
+    // const [myState] = useAgile([MY_STATE]);
     const [test, setTest] = useState(false);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-            {Test}
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <button onClick={() => setTimeout(() => {MY_STATE.set("Test2");}, 3000)}>
+                    {props.myState}_{props.myState2}
+                </button>
+            </header>
+        </div>
+    );
 }
 
-export default App;
+export default AgileHOC(App, {myState: MY_STATE, myState2: MY_STATE_2});
--- examples/react-typescript/src/core/index.ts
+++ examples/react-typescript/src/core/index.ts
@@ -6,7 +6,12 @@ export const Test = 'Test';
 console.log("Test");
 
 export const App = new Agile({
-    framework: React
+    logJobs: true,
 });
 
-// export const MY_STATE = App.State<string>("Test");
+export const MY_STATE = App.State<string>("MyState").key("my_state");
+export const MY_STATE_2 = App.State<string>("MyState2").key("my_state_2");
+
+MY_STATE.watch("test", (value) => {
+    console.log("Watch " + value);
+})
