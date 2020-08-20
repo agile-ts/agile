e9438ee5532d88ff558e40684f5c6e7454608e72
BennoDev <bennokohrs@gmail.com>
2020-08-20 22:00:04 +0200

    created api

--- src/utils.ts
+++ src/utils.ts
@@ -66,3 +66,21 @@ export function isFunction(func: () => any) {
 export function isAsyncFunction(func: () => any) {
     return func.constructor.name === 'AsyncFunction';
 }
+
+
+//=========================================================================================================
+// Is Valid Url
+// https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+//=========================================================================================================
+/**
+ * Checks if url is valid
+ */
+export function isValidUrl(url: string): boolean {
+    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return pattern.test(url);
+}
