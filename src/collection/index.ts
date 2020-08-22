4c7fe04275299956dd39dddb488aae79683fa76c
BennoDev <bennokohrs@gmail.com>
2020-08-22 12:54:03 +0200

    Created init subInstance method which inits for example the groups out of the config

--- src/collection/index.ts
+++ src/collection/index.ts
@@ -7,8 +7,8 @@ import {defineConfig} from "../utils";
 export type DefaultDataItem = { [key: string]: any };
 
 export interface CollectionConfig {
-    groups?: { [key: string]: Group<any> }
-    selectors?: { [key: string]: Selector<any> }
+    groups?: { [key: string]: Group<any> } | string[]
+    selectors?: { [key: string]: Selector<any> } | string[]
     key?: string
     primaryKey?: string
     indexAll?: boolean
@@ -29,8 +29,8 @@ export class Collection<DataType = DefaultDataItem> {
     // Collection data is stored here
     public data: { [key: string]: Data<DataType> } = {};
 
-    // public groups: this['config']['groups'];
-    // public selectors: this['config']['selectors'];
+    public groups: { [key: string]: Group<any> } = {};
+    public selectors: { [key: string]: Selector<any> } = {};
 
     constructor(agileInstance: Agile, config: Config<DataType>) {
         this.agileInstance = () => agileInstance;
@@ -45,6 +45,60 @@ export class Collection<DataType = DefaultDataItem> {
             groups: {},
             selectors: {}
         });
+
+        // Create Groups
+        if (config.groups)
+            this.initSubInstances('groups');
+
+        // Create Selectors
+        if (config.selectors)
+            this.initSubInstances('selectors');
+    }
+
+
+    //=========================================================================================================
+    // Init SubInstances
+    //=========================================================================================================
+    /**
+     * Init SubInstances like groups or selectors
+     */
+    private initSubInstances(type: 'groups' | 'selectors') {
+        const finalSubInstanceObject: any = {};
+        let subInstanceObject: any = {};
+        const subInstance = this.config[type];
+
+        // Return empty object if subInstance doesn't exists
+        if (!subInstance) return {};
+
+        // if subInstance is array transform it to an object with the fitting class
+        if (Array.isArray(subInstance)) {
+            for (let i = 0; i < subInstance.length; i++) {
+                let instance;
+                switch (type) {
+                    case "groups":
+                        instance = new Group();
+                        break;
+                    case "selectors":
+                        instance = new Selector();
+                        break;
+                    default:
+                        instance = 'unknown';
+                }
+                subInstanceObject[subInstance[i]] = instance;
+            }
+        } else {
+            subInstanceObject = subInstance;
+        }
+
+        // Loop through subInstance items
+        const keys = Object.keys(subInstanceObject);
+        for (let i = 0; i < keys.length; i++) {
+            // Create the sub instance in the final subInstance object
+            finalSubInstanceObject[keys[i]] = subInstanceObject[keys[i]];
+        }
+
+        // Set Collection instance
+        this[type] = finalSubInstanceObject;
     }
 
 
