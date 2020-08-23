0fbbb6eb6c62f8dc979e42dc20ce90a76676a1ef
BennoDev <bennokohrs@gmail.com>
2020-08-23 15:17:33 +0200

    Updated Example

--- examples/react-typescript/src/App.tsx
+++ examples/react-typescript/src/App.tsx
@@ -2,37 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {useAgile} from 'agile-framework';
-import {MY_COMPUTED, MY_STATE, MY_STATE_2} from "./core";
+import {MY_COLLECTION, MY_COMPUTED, MY_STATE, MY_STATE_2} from "./core";
 
 const App = (props: any) => {
 
     const [myComputed] = useAgile([MY_COMPUTED]);
     const [myState, myState2] = useAgile([MY_STATE, MY_STATE_2]);
+    const [myCollection] = useAgile([MY_COLLECTION.getGroup('myGroup')]);
 
 
     console.log("myComputed", MY_COMPUTED);
     console.log("myState", MY_STATE);
+    console.log("myState2", MY_STATE_2);
+    console.log("myCollection", MY_COLLECTION);
+
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <button onClick={() => setTimeout(() => {
-                    MY_STATE.set("Test4");
-                }, 1000)}>
-                    {myState}_{myState2}
-                </button>
-                <button onClick={() => setTimeout(() => {
-                    MY_STATE_2.set("Test6");
-                }, 1000)}>
-                    {myState}_{myState2}
-                </button>
-                <p>
-                    {myComputed}
-                </p>
+
+                <div className={"Container"}>
+                    <h3 className={"Title"}>My State</h3>
+                    <button onClick={() => setTimeout(() => {
+                        MY_STATE.set("Test1");
+                    }, 1000)}>
+                        {myState}_{myState2}
+                    </button>
+                </div>
+
+                <div className={"Container"}>
+                    <h3 className={"Title"}>My State_2</h3>
+                    <button onClick={() => setTimeout(() => {
+                        MY_STATE_2.set("Test3");
+                    }, 1000)}>
+                        {myState}_{myState2}
+                    </button>
+                </div>
+
+                <div className={"Container"}>
+                    <h3 className={"Title"}>My Computed</h3>
+                    <p>
+                        {myComputed}
+                    </p>
+                </div>
+
+                <div className={"Container"}>
+                    <h3 className={"Title"}>My Collection</h3>
+                    <div>
+                        {
+                            myCollection.map((item: any) => <p>{item.name}</p>)
+                        }
+                    </div>
+                    <button onClick={() => setTimeout(() => {
+                        MY_COLLECTION.collect({id: 'id3', name: 'Test3'});
+                    }, 1000)}>
+                        Collect
+                    </button>
+                </div>
+
             </header>
         </div>
     );
--- examples/react-typescript/src/core/index.ts
+++ examples/react-typescript/src/core/index.ts
@@ -13,34 +13,32 @@ MY_STATE.watch("test", (value) => {
     console.log("Watch " + value);
 });
 
+
 export const MY_COMPUTED = App.Computed<string>(() => {
     return "test" + MY_STATE.value + "_computed_" + MY_STATE_2.value;
 });
-
 
 interface collectionValueInterface {
     id: string,
     name: string
 }
 
-// @ts-ignore
 export const MY_COLLECTION = App.Collection<collectionValueInterface>(collection => ({
     key: 'my-collection',
     groups: {
         myGroup: collection.Group()
     }
 }));
-MY_COLLECTION.collect({id: '1', name: 'test'});
-MY_COLLECTION.collect({id: '2', name: 'test2'}, 'myGroup');
-console.log("Group: myCollection ", MY_COLLECTION);
+MY_COLLECTION.collect({id: 'id1', name: 'test'});
+MY_COLLECTION.collect({id: 'id2', name: 'test2'}, 'myGroup');
+console.log("Initial: myCollection ", MY_COLLECTION);
 
 export const MY_COLLECTION_2 = App.Collection<collectionValueInterface>({
     key: 'my-collection_2',
     groups: ['myGroup']
 });
-MY_COLLECTION_2.collect({id: '1', name: 'test'});
-MY_COLLECTION_2.collect({id: '2', name: 'test2'}, 'myGroup');
-console.log("Group: myCollection2 ", MY_COLLECTION_2);
-
+MY_COLLECTION_2.collect({id: 'id1', name: 'test'});
+MY_COLLECTION_2.collect({id: 'id2', name: 'test2'}, 'myGroup');
+console.log("Initial: myCollection2 ", MY_COLLECTION_2);
 
 // console.log(JSON.stringify(MY_STATE));
