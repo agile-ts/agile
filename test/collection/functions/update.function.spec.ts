9ea4d34fa0f799a9b7bfcc837122f2891811866d
Benno Kohrs <bennokohrs@gmail.com>
2020-09-05 22:03:47 +0200

    Made some small tweaks in update function tests

--- test/collection/functions/update.function.spec.ts
+++ test/collection/functions/update.function.spec.ts
@@ -29,48 +29,33 @@ describe('Update Function Tests', () => {
     // Create Collection
     const MY_COLLECTION = App.Collection<userInterface>();
 
-    // Collect some items
-    MY_COLLECTION.collect({id: 1, name: 'jeff'}, [], {background: true});
-    MY_COLLECTION.collect({id: 2, name: 'jamie'}, [], {background: true});
-    MY_COLLECTION.collect({id: 3, name: 'hans'}, [], {background: true});
-
     // Set 'Hook' for testing the rerenderFunctionality with the callbackFunction (Note: the value of myHookState doesn't get changed because no rerenders happen -> no reassign of the value)
     const [myHookCollection] = useTest([MY_COLLECTION], () => {
         rerenderCount++;
     });
 
+    // Collect some items
+    MY_COLLECTION.collect([{id: 1, name: 'jeff'}, {id: 2, name: 'jamie'}, {id: 3, name: 'hans'}])
+
     it('Has correct initial values', () => {
         expect(MY_COLLECTION.groups['default'] instanceof Group).to.eq(true, 'MY_COLLECTION default Group has been created')
         expect(MY_COLLECTION.groups['default']?.dep.subs.size === 1).to.eq(true, 'MY_COLLECTION default Group has correct subs size');
-        expect(MY_COLLECTION.size).to.eq(3, 'MY_COLLECTION size is 3 because of 3 default collects');
+        expect(MY_COLLECTION.size).to.eq(3, 'MY_COLLECTION has correct size');
         expect(JSON.stringify(MY_COLLECTION.data[1].value)).to.eq(JSON.stringify({
             id: 1,
             name: 'jeff'
-        }), 'MY_COLLECTION item at id 1 is correct');
+        }), 'MY_COLLECTION item at id 1 has correct value');
         expect(JSON.stringify(MY_COLLECTION.data[2].value)).to.eq(JSON.stringify({
             id: 2,
             name: 'jamie'
-        }), 'MY_COLLECTION item at id 2 is correct');
+        }), 'MY_COLLECTION item at id 2 has correct value');
         expect(JSON.stringify(MY_COLLECTION.data[3].value)).to.eq(JSON.stringify({
             id: 3,
             name: 'hans'
-        }), 'MY_COLLECTION item at id 3 is correct');
+        }), 'MY_COLLECTION item at id 3 has correct value');
 
-        expect(JSON.stringify(myHookCollection)).to.eq(JSON.stringify([
-            {
-                id: 1,
-                name: 'jeff'
-            },
-            {
-                id: 2,
-                name: 'jamie'
-            },
-            {
-                id: 3,
-                name: 'hans'
-            }
-        ]), 'myHookState has correct MY_COLLECTION value');
-        expect(rerenderCount).to.eq(0, 'rerenderCount is 0');
+        expect(JSON.stringify(myHookCollection)).to.eq(JSON.stringify([]), 'myHookState has correct MY_COLLECTION value');
+        expect(rerenderCount).to.eq(1, 'rerenderCount has correct value');
     });
 
     it('Can update Item', async () => {
@@ -86,7 +71,7 @@ describe('Update Function Tests', () => {
         }), 'MY_COLLECTION data at id 1 has updated');
         expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-        expect(rerenderCount).to.eq(1, 'rerenderCount has been increased by 1');
+        expect(rerenderCount).to.eq(2, 'rerenderCount has been increased by 1');
     });
 
     it('Can update Item and primaryKey', async () => {
@@ -105,7 +90,7 @@ describe('Update Function Tests', () => {
         expect(MY_COLLECTION.groups["default"].value.findIndex(value => value === 3) !== -1).to.eq(false, 'MY_COLLECTION item with id 3 doesn\'t exist in default group anymore');
         expect(MY_COLLECTION.groups["default"].value.findIndex(value => value === 4) !== -1).to.eq(true, 'MY_COLLECTION item with id 3 does exist in default group');
 
-        expect(rerenderCount).to.eq(2, 'rerenderCount has been increased by 1');
+        expect(rerenderCount).to.eq(3, 'rerenderCount has been increased by 1');
     });
 
     it('Can\'t update not existing Item', async () => {
@@ -117,7 +102,7 @@ describe('Update Function Tests', () => {
 
         expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items');
 
-        expect(rerenderCount).to.eq(2, 'rerenderCount has stayed the same');
+        expect(rerenderCount).to.eq(3, 'rerenderCount has stayed the same');
     });
 
     it('Can\'t update Item with the same value', async () => {
@@ -133,7 +118,7 @@ describe('Update Function Tests', () => {
         }), 'MY_COLLECTION data at id 1 stayed the same');
         expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-        expect(rerenderCount).to.eq(2, 'rerenderCount has stayed the same');
+        expect(rerenderCount).to.eq(3, 'rerenderCount has stayed the same');
     });
 
     it('Can\'t update Item with non object changes', async () => {
@@ -146,7 +131,7 @@ describe('Update Function Tests', () => {
 
         expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items');
 
-        expect(rerenderCount).to.eq(2, 'rerenderCount has stayed the same');
+        expect(rerenderCount).to.eq(3, 'rerenderCount has stayed the same');
     });
 
     describe('Test background option', () => {
@@ -163,7 +148,7 @@ describe('Update Function Tests', () => {
             }), 'MY_COLLECTION data at id 2 has updated');
             expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-            expect(rerenderCount).to.eq(3, 'rerenderCount has been increased by 1');
+            expect(rerenderCount).to.eq(4, 'rerenderCount has been increased by 1');
         });
 
         it('Doesn\'t call callBackFunction by updating Item with background = true', async () => {
@@ -179,7 +164,7 @@ describe('Update Function Tests', () => {
             }), 'MY_COLLECTION data at id 4 has updated');
             expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-            expect(rerenderCount).to.eq(3, 'rerenderCount stayed the same');
+            expect(rerenderCount).to.eq(4, 'rerenderCount stayed the same');
         });
 
         it('Doesn\'t call callBackFunction by updating Item and primaryKey with background = true', async () => {
@@ -195,7 +180,7 @@ describe('Update Function Tests', () => {
             }), 'MY_COLLECTION data switched from id 4 to id 3 and has updated');
             expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-            expect(rerenderCount).to.eq(3, 'rerenderCount stayed the same');
+            expect(rerenderCount).to.eq(4, 'rerenderCount stayed the same');
         });
     });
 
@@ -213,7 +198,7 @@ describe('Update Function Tests', () => {
             }), 'MY_COLLECTION data at id 2 has updated');
             expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-            expect(rerenderCount).to.eq(4, 'rerenderCount has been increased by 1');
+            expect(rerenderCount).to.eq(5, 'rerenderCount has been increased by 1');
         });
 
 
@@ -231,7 +216,7 @@ describe('Update Function Tests', () => {
             }), 'MY_COLLECTION data at id 2 has updated');
             expect(Object.keys(MY_COLLECTION.data).length).to.eq(3, 'MY_COLLECTION has still all items')
 
-            expect(rerenderCount).to.eq(5, 'rerenderCount has been increased by 1');
+            expect(rerenderCount).to.eq(6, 'rerenderCount has been increased by 1');
         });
     });
 });
