3f7b6aceeada18800178924568119c803db61966
Benno Kohrs <bennokohrs@gmail.com>
2020-10-30 22:11:11 +0100

    Added yalc to test Agile in react-typescript without having issues with the devDependencies of Agile (two instances of React)

--- packages/react/src/hooks/useEvent.ts
+++ packages/react/src/hooks/useEvent.ts
@@ -1,9 +1,12 @@
 import React from "react";
-import {Event, EventCallbackFunction} from '@agile-ts/core';
+import { Event, EventCallbackFunction } from "@agile-ts/core";
 
-export function useEvent<E extends Event>(event: E, callback: EventCallbackFunction<E['payload']>) {
-    // Call event on component mount and remove event on component unmount
-    React.useEffect(function () {
-        return event.on(callback);
-    }, []);
+export function useEvent<E extends Event>(
+  event: E,
+  callback: EventCallbackFunction<E["payload"]>
+) {
+  // Call event on component mount and remove event on component unmount
+  React.useEffect(function () {
+    return event.on(callback);
+  }, []);
 }
