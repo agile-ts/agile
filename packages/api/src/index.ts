5113b3da89808211f4d6262c6d05d288660dd61b
Benno Kohrs <bennokohrs@gmail.com>
2020-11-02 20:53:52 +0100

    Refactored Api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,88 +1,104 @@
 import * as http from "http";
-import { copy, isValidUrl } from "@agile-ts/core";
-
-//=========================================================================================================
-// API
-//=========================================================================================================
-/**
- * Create Agile API
- * @param config Object
- * @param config.options Object - Typescript default: RequestInit (headers, credentials, mode, etc...)
- * @param config.baseURL String - Url to prepend to endpoints (without trailing slash)
- * @param config.timeout Number - Time to wait for request before throwing error
- */
-// public API = (config: apiConfig) => new API(config);
-
-export interface AgileResponse<DataType = any> {
-  data: DataType; // request body data
-  timedout?: boolean; // if request get timedout
-  status: number; // request status code
-  raw?: Response; // the raw response
-  type?: string | null; // response type (for instance 'application/json')
-}
-
-export interface apiConfig {
-  options: RequestInit; // Request Options
-  baseURL?: string; // baseURL (https://api.mysite.co)
-  path?: string; // path (api) -> combined with baseUrl the url is https://api.mysite.co/api/...
-  timeout?: number; // when the request should timeout
-}
+import {
+  clone,
+  copy,
+  defineConfig,
+  isValidObject,
+  isValidUrl,
+} from "@agile-ts/core";
 
 export default class API {
-  public config: apiConfig;
+  public config: ApiConfig;
 
-  constructor(config: apiConfig = { options: {} }) {
+  /**
+   * @public
+   * @param config - Config
+   */
+  constructor(config: ApiConfig = { options: {} }) {
     this.config = config;
+  }
+
+  //=========================================================================================================
+  // With
+  //=========================================================================================================
+  /**
+   * Overrides API Config and returns overwritten API Instance
+   * Note: Doesn't overwrite this API Class
+   * @param config - Config
+   */
+  public with(config: ApiConfig): API {
+    const modifiedApi = clone(this);
+    modifiedApi.config = { ...this.config, ...config };
+    return modifiedApi;
   }
 
   //=========================================================================================================
   // Get
   //=========================================================================================================
   /**
-   *  Get request
+   * @public
+   *  Get Request
+   *  @param path - Url Path
+   *  @param config - Config
    */
-  public get(endpoint: string, options?: RequestInit) {
-    return this.send("GET", endpoint, options);
+  public get(path: string, config: RequestInit = {}) {
+    return this.send("GET", path, config);
   }
 
   //=========================================================================================================
   // Post
   //=========================================================================================================
   /**
-   *  Post request
+   * @public
+   *  Post Request
+   *  @param path - Url Path
+   *  @param payload - Payload of Post Request
+   *  @param config - Config
    */
-  public post(endpoint: string, payload?: any, options?: RequestInit) {
-    return this.send("POST", endpoint, payload, options);
+  public post(path: string, payload?: any, config: RequestInit = {}) {
+    return this.send("POST", path, payload, config);
   }
 
   //=========================================================================================================
   // Put
   //=========================================================================================================
   /**
-   *  Put request
+   * @public
+   *  Put Request
+   *  @param path - Url Path
+   *  @param payload - Payload of Put Request
+   *  @param config - Config
    */
-  public put(endpoint: string, payload?: any, options?: RequestInit) {
-    return this.send("PUT", endpoint, payload, options);
+  public put(path: string, payload?: any, config: RequestInit = {}) {
+    return this.send("PUT", path, payload, config);
   }
 
   //=========================================================================================================
   // Patch
   //=========================================================================================================
   /**
-   *  Patch request
+   * @public
+   *  Patch Request
+   *  @param path - Url Path
+   *  @param payload - Payload of Patch Request
+   *  @param config - Config
    */
-  public patch(endpoint: string, payload?: any, options?: RequestInit) {
-    return this.send("PATCH", endpoint, payload, options);
+  public patch(path: string, payload?: any, config: RequestInit = {}) {
+    return this.send("PATCH", path, payload, config);
   }
 
   //=========================================================================================================
   // Delete
   //=========================================================================================================
   /**
-   *  Delete request
+   * @public
+   *  Delete Request
+   *  @param path - Url Path
+   *  @param payload - Payload of Delete Request
+   *  @param config - Config
    */
-  public delete(endpoint: string, payload?: any, options?: RequestInit) {
-    return this.send("DELETE", endpoint, payload, options);
+  public delete(path: string, payload?: any, config: RequestInit = {}) {
+    return this.send("DELETE", path, payload, config);
   }
 
   //=========================================================================================================
@@ -90,86 +106,75 @@ export default class API {
   //=========================================================================================================
   /**
    * @internal
-   *  Will handle the request
+   * Handles Requests
    */
   private async send(
     method: string,
     endpoint: string,
     payload?: any,
-    options?: RequestInit
+    options: RequestInit = {}
   ): Promise<AgileResponse> {
     let fullUrl: string;
     let response: Response | undefined;
     let timedout = false;
-    const config: apiConfig = copy(this.config); // Copying config because it will adapted for every specific request
 
-    // Merge options together
-    if (options) config.options = { ...config.options, ...options };
-
-    // Inject method into request options
+    // Confige Config
+    const config: ApiConfig = copy(this.config);
+    config.options = defineConfig(options, config.options);
     config.options.method = method;
-
-    // If no header set it to an empty object (because in some conditions we have to add some stuff to this object)
-    // Haven't found a way by doing it with 'Headers'(https://stackoverflow.com/questions/48798236/const-initialization-error-with-requestinit-in-typescript)
     if (!config.options.headers) config.options.headers = {};
 
     // Set Body
-    if (typeof payload === "object") {
-      // Set body to stringyfied object payload
+    if (isValidObject(payload)) {
       config.options.body = JSON.stringify(payload);
-
-      // Set content type of Header to json
-      // @ts-ignore
       config.options.headers["content-type"] = "application/json";
     } else {
-      // Set body to payload
       config.options.body = payload;
     }
 
-    // Construct endpoint (fullUrl)
-    let path = this.config.path ? "/" + this.config.path : "";
+    // Build Url
     if (endpoint.startsWith("http")) fullUrl = endpoint;
     else
-      fullUrl = `${
-        this.config.baseURL ? this.config.baseURL : ""
-      }${path}/${endpoint}`;
+      fullUrl = `${this.config.baseURL || ""}${
+        "/" + this.config.path || ""
+      }/${endpoint}`;
 
-    // Warning if fullUrl might not be valid
-    if (!isValidUrl(fullUrl)) console.warn("No valid url ", fullUrl);
+    // Warning if fullUrl might be invalid
+    if (!isValidUrl(fullUrl))
+      console.warn(`Url '${fullUrl}' might be invalid!`);
 
-    // Send Request with timeout
+    // Send Request with Timeout
     if (this.config.timeout) {
-      let t: any;
+      let timeout: any;
 
-      // Create timeout Promise
-      const timeout = new Promise((resolve) => {
-        t = setTimeout(() => {
+      // Create Timeout Promise
+      const timeoutPromise = new Promise((resolve) => {
+        timeout = setTimeout(() => {
           timedout = true;
           resolve(undefined);
         }, this.config.timeout);
       });
 
-      // Create request Promise
+      // Send Request Promise
       const request = new Promise((resolve, reject) => {
         fetch(fullUrl, this.config.options)
           .then((data) => {
-            // Clear Timeout
-            clearTimeout(t);
-
-            // Resolve Response(data)
+            clearTimeout(timeout);
             resolve(data);
           })
           .catch(reject);
       });
-      // @ts-ignore (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/race)
-      response = await Promise.race([timeout, request]);
-    } else {
-      // Send Request without timeout
-      response = await fetch(fullUrl, this.config.options);
+
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/race
+      response = (await Promise.race([timeoutPromise, request])) as any;
     }
 
-    // Create FinalResponse
-    let finalResponse: AgileResponse = {
+    // Send Request without Timeout
+    if (!this.config.timeout)
+      response = await fetch(fullUrl, this.config.options);
+
+    // Create Agile Response
+    const agileResponse: AgileResponse = {
       status: timedout ? 408 : response?.status || 404,
       raw: response,
       data: {},
@@ -177,12 +182,38 @@ export default class API {
       timedout: timedout,
     };
 
-    // Extract response data
-    if (finalResponse.type?.includes("application/json"))
-      finalResponse.data = await finalResponse.raw?.json();
-    else if (typeof finalResponse.raw?.text === "function")
-      finalResponse.data = await finalResponse.raw.text();
+    // Extract Response Data
+    if (agileResponse.type?.includes("application/json"))
+      agileResponse.data = await agileResponse.raw?.json();
 
-    return finalResponse;
+    return agileResponse;
   }
+}
+
+/**
+ * @param data - Request Body Data
+ * @param timedout - If Request timed out
+ * @param status - Response Status Code
+ * @param raw - Raw Response
+ * @param type - Response Type for instance 'application/json'
+ */
+export interface AgileResponse<DataType = any> {
+  data: DataType;
+  timedout?: boolean;
+  status: number;
+  raw?: Response;
+  type?: string | null;
+}
+
+/**
+ * @param options - Request Options
+ * @param baseURL - Base Url of the Endpoint (eg 'https://api.mysite.co')
+ * @param path - Path to the Endpoint (eg '/v1')
+ * @param timeout - Timeout if Endpoint didn't Response
+ */
+export interface ApiConfig {
+  options: RequestInit;
+  baseURL?: string;
+  path?: string;
+  timeout?: number;
 }
