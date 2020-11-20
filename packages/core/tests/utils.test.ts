dcbdf0b9b15a2dd9d92ed61b7d220acd70869a7b
Benno Kohrs <bennokohrs@gmail.com>
2020-11-20 21:47:05 +0100

    Created more copy tests

--- packages/core/tests/utils.test.ts
+++ packages/core/tests/utils.test.ts
@@ -13,5 +13,29 @@ describe("Utils", () => {
       expect(myCopiedArray).toStrictEqual([1, 2, 3, 4, 5, 6]);
       expect(myArray).toStrictEqual([1, 2, 3, 4, 5]);
     });
+
+    it("should copy Object without any reference", () => {
+      const myObject = { id: 1, name: "jeff" };
+      const myCopiedObject = copy(myObject);
+
+      expect(myCopiedObject).toStrictEqual({ id: 1, name: "jeff" });
+
+      myCopiedObject.name = "hans";
+
+      expect(myCopiedObject).toStrictEqual({ id: 1, name: "hans" });
+      expect(myObject).toStrictEqual({ id: 1, name: "jeff" });
+    });
+
+    it("should copy default Types", () => {
+      const myNumber = 5;
+      const myCopiedNumber = copy(myNumber);
+      const myString = "frank";
+      const myCopiedString = copy(myString);
+
+      expect(myCopiedNumber).toBe(5);
+      expect(myCopiedString).toBe("frank");
+    });
   });
+
+  describe("isValidObject", () => {});
 });
