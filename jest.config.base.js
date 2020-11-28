cb8bc44d459a767447b19729a84436e73b73b783
Benno Kohrs <bennokohrs@gmail.com>
2020-11-28 08:34:45 +0100

    Updated jest config

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,8 +2,8 @@ module.exports = {
   testEnvironment: "node",
   coveragePathIgnorePatterns: ["(tests/.*.mock).(jsx?|tsx?)$"],
   modulePathIgnorePatterns: ["dist"],
+  testMatch: ["<rootDir>/packages/**/tests/**/*.test.ts"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  testMatch: ["**/*.test.*"],
 };
