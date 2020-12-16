f044de741f152cc15b64248c2c647d65ff1d15ec
Benno Kohrs <bennokohrs@gmail.com>
2020-12-16 18:08:18 +0100

    Created basic computed tests

--- packages/core/tests/new/computed/computed.test.ts
+++ packages/core/tests/new/computed/computed.test.ts
@@ -1,4 +1,11 @@
-import { Computed, Agile, StateObserver, Observer, State } from "../../../src";
+import {
+  Computed,
+  Agile,
+  StateObserver,
+  Observer,
+  State,
+  ComputedTracker,
+} from "../../../src";
 
 describe("Computed Tests", () => {
   let dummyAgile: Agile;
@@ -165,7 +172,7 @@ describe("Computed Tests", () => {
         });
       });
 
-      it("should updated computeFunction and overwrite hardCodedDeps (config.overwriteDeps = false)", () => {
+      it("should updated computeFunction and add additional deps to hardCodedDeps (config.overwriteDeps = false)", () => {
         computed.updateComputeFunction(
           newComputeFunction,
           [dummyState, dummyObserver],
@@ -187,6 +194,102 @@ describe("Computed Tests", () => {
           background: false,
           sideEffects: true,
         });
+      });
+    });
+
+    describe("computeValue function tests", () => {
+      let dummyObserver1: Observer;
+      let dummyObserver2: Observer;
+      let dummyObserver3: Observer;
+
+      beforeEach(() => {
+        dummyObserver1 = new Observer(dummyAgile);
+        dummyObserver2 = new Observer(dummyAgile);
+        dummyObserver3 = new Observer(dummyAgile);
+        dummyObserver1.depend = jest.fn();
+        dummyObserver2.depend = jest.fn();
+        dummyObserver3.depend = jest.fn();
+        ComputedTracker.track = jest.fn();
+        computed.hardCodedDeps = [dummyObserver3];
+      });
+
+      it("should call computeFunction and track dependencies the computeFunction depends on", () => {
+        computed.computeFunction = jest.fn(() => "newComputedValue");
+        ComputedTracker.getTrackedObservers = jest.fn(() => [
+          dummyObserver1,
+          dummyObserver2,
+        ]);
+
+        const response = computed.computeValue();
+
+        expect(response).toBe("newComputedValue");
+        expect(dummyComputeFunction).toHaveBeenCalled();
+        expect(ComputedTracker.track).toHaveBeenCalled();
+        expect(ComputedTracker.getTrackedObservers).toHaveBeenCalled();
+        expect(computed.hardCodedDeps).toStrictEqual([dummyObserver3]);
+        expect(computed.deps).toStrictEqual([
+          dummyObserver3,
+          dummyObserver1,
+          dummyObserver2,
+        ]);
+        expect(dummyObserver1.depend).toHaveBeenCalledWith(computed.observer);
+        expect(dummyObserver2.depend).toHaveBeenCalledWith(computed.observer);
+        expect(dummyObserver3.depend).toHaveBeenCalledWith(computed.observer);
+      });
+    });
+
+    describe("formatDeps function tests", () => {
+      let dummyObserver: Observer;
+      let dummyStateObserver: StateObserver;
+      let dummyState: State;
+
+      beforeEach(() => {
+        dummyObserver = new Observer(dummyAgile);
+        dummyState = new State(dummyAgile, undefined);
+        dummyStateObserver = new StateObserver(dummyState);
+        dummyState.observer = dummyStateObserver;
+      });
+
+      it("should return Observers that could be extracted from the passed Instances", () => {
+        const response = computed.formatDeps([
+          dummyObserver,
+          dummyState,
+          undefined,
+          {},
+          { observer: "fake" },
+        ]);
+
+        expect(response).toStrictEqual([dummyObserver, dummyStateObserver]);
+      });
+    });
+
+    describe("patch function tests", () => {
+      it("should print error", () => {
+        computed.patch();
+
+        expect(console.error).toHaveBeenCalledWith(
+          "Agile Error: You can't use patch method on ComputedState!"
+        );
+      });
+    });
+
+    describe("persist function tests", () => {
+      it("should print error", () => {
+        computed.persist();
+
+        expect(console.error).toHaveBeenCalledWith(
+          "Agile Error: You can't use persist method on ComputedState!"
+        );
+      });
+    });
+
+    describe("invert function tests", () => {
+      it("should print error", () => {
+        computed.invert();
+
+        expect(console.error).toHaveBeenCalledWith(
+          "Agile Error: You can't use invert method on ComputedState!"
+        );
       });
     });
   });
