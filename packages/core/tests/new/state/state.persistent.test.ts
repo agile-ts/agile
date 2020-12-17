c40f0699a2c07aa68d3d7245511a850ef7b519e7
Benno Kohrs <bennokohrs@gmail.com>
2020-12-17 14:11:35 +0100

    refactored state persistent tests

--- packages/core/tests/new/state/state.persistent.test.ts
+++ packages/core/tests/new/state/state.persistent.test.ts
@@ -2,8 +2,8 @@ import {
   Agile,
   State,
   StatePersistent,
-  Persistent,
   Storage,
+  Persistent,
 } from "../../../src";
 
 describe("StatePersistent Tests", () => {
@@ -11,108 +11,106 @@ describe("StatePersistent Tests", () => {
   let dummyState: State;
 
   beforeEach(() => {
+    jest.clearAllMocks();
     console.error = jest.fn();
     dummyAgile = new Agile({ localStorage: false });
     dummyState = new State(dummyAgile, "dummyValue");
+    jest.spyOn(StatePersistent.prototype, "instantiatePersistent");
+    jest.spyOn(StatePersistent.prototype, "initialLoading");
   });
 
-  it("should create StatePersistent (default config)", () => {
+  it("should create StatePersistent and shouldn't call initialLoading if Persistent isn't ready (default config)", () => {
+    // Overwrite instantiatePersistent once to not call it and set ready property
+    jest
+      .spyOn(StatePersistent.prototype, "instantiatePersistent")
+      .mockImplementationOnce(function () {
+        this.ready = false;
+      });
+
     const statePersistent = new StatePersistent(dummyState);
 
     expect(statePersistent).toBeInstanceOf(StatePersistent);
     expect(statePersistent.state()).toBe(dummyState);
-    /* Couldn't figure out how to mock anything in the Constructor
-        expect(Persistent).toHaveBeenCalledWith(dummyAgile, {
-          instantiate: false,
-        });
-        expect(statePersistent.instantiatePersistent).toHaveBeenCalledWith({
-          key: undefined,
-          storageKeys: [],
-        });
-        expect(statePersistent.initialLoading).not.toHaveBeenCalled();
-    */
-    expect(statePersistent.key).toBe(Persistent.placeHolderKey);
+    expect(statePersistent.instantiatePersistent).toHaveBeenCalledWith({
+      key: undefined,
+      storageKeys: [],
+    });
+    expect(statePersistent.initialLoading).not.toHaveBeenCalled();
+
+    expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
     expect(statePersistent.ready).toBeFalsy();
     expect(statePersistent.isPersisted).toBeFalsy();
     expect(statePersistent.onLoad).toBeUndefined();
     expect(statePersistent.storageKeys).toStrictEqual([]);
     expect(statePersistent.defaultStorageKey).toBeUndefined();
-
-    expect(console.error).toHaveBeenCalledWith(
-      "Agile Error: No valid persist Key found! Please provide a Key or assign one to the parent instance."
-    );
-
-    // Sleep 5ms because initialLoading happens async
-    return new Promise((resolve) => setTimeout(resolve, 5)).then(() => {
-      expect(dummyState.isPersisted).toBeFalsy();
-    });
   });
 
-  it("should create valid StatePersistent (config.key, config.storageKeys)", () => {
+  it("should create StatePersistent and should call initialLoading if Persistent is ready (default config)", () => {
+    // Overwrite instantiatePersistent once to not call it
+    jest
+      .spyOn(StatePersistent.prototype, "instantiatePersistent")
+      .mockImplementationOnce(function () {
+        this.ready = true;
+      });
+
+    const statePersistent = new StatePersistent(dummyState);
+
+    expect(statePersistent.initialLoading).toHaveBeenCalled();
+  });
+
+  it("should create StatePersistent and shouldn't call initialLoading if Persistent isn't ready (specific config)", () => {
+    // Overwrite instantiatePersistent once to not call it and set ready property
+    jest
+      .spyOn(StatePersistent.prototype, "instantiatePersistent")
+      .mockImplementationOnce(function () {
+        this.ready = false;
+      });
+
     const statePersistent = new StatePersistent(dummyState, {
       key: "statePersistentKey",
       storageKeys: ["test1", "test2"],
     });
 
     expect(statePersistent).toBeInstanceOf(StatePersistent);
-    /* Couldn't figure out how to mock anything in the Constructor
-        expect(Persistent).toHaveBeenCalledWith(dummyAgile, {
-          instantiate: false,
-        });
-        expect(statePersistent.instantiatePersistent).toHaveBeenCalledWith({
-          key: "statePersistentKey",
-          storageKeys: ["test1", "test2"],
-        });
-        expect(statePersistent.initialLoading).toHaveBeenCalled();
-    */
-
-    expect(statePersistent.key).toBe("statePersistentKey"); // x
-    expect(statePersistent.ready).toBeTruthy(); // x
-    expect(statePersistent.isPersisted).toBeFalsy();
-    expect(statePersistent.onLoad).toBeUndefined();
-    expect(statePersistent.storageKeys).toStrictEqual(["test1", "test2"]); // x
-    expect(statePersistent.defaultStorageKey).toBe("test1"); // x
-
-    // expect(console.error).not.toHaveBeenCalled();
-
-    // Sleep 5ms because initialLoading happens async
-    return new Promise((resolve) => setTimeout(resolve, 5)).then(() => {
-      expect(dummyState.isPersisted).toBeTruthy(); // x
-    });
-  });
-
-  it("should create valid StatePersistent (config.key, config.storageKeys, config.instantiate = false)", () => {
-    const statePersistent = new StatePersistent(dummyState, {
+    expect(statePersistent.instantiatePersistent).toHaveBeenCalledWith({
       key: "statePersistentKey",
       storageKeys: ["test1", "test2"],
+    });
+    expect(statePersistent.initialLoading).not.toHaveBeenCalled();
+
+    expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
+    expect(statePersistent.ready).toBeFalsy();
+    expect(statePersistent.isPersisted).toBeFalsy();
+    expect(statePersistent.onLoad).toBeUndefined();
+    expect(statePersistent.storageKeys).toStrictEqual([]);
+    expect(statePersistent.defaultStorageKey).toBeUndefined();
+  });
+
+  it("should create StatePersistent and shouldn't call initialLoading if Persistent is ready (config.instantiate = false)", () => {
+    // Overwrite instantiatePersistent once to not call it and set ready property
+    jest
+      .spyOn(StatePersistent.prototype, "instantiatePersistent")
+      .mockImplementationOnce(function () {
+        this.ready = true;
+      });
+
+    const statePersistent = new StatePersistent(dummyState, {
       instantiate: false,
     });
 
     expect(statePersistent).toBeInstanceOf(StatePersistent);
-    /* Couldn't figure out how to mock anything in the Constructor
-        expect(Persistent).toHaveBeenCalledWith(dummyAgile, {
-          instantiate: false,
-        });
-        expect(statePersistent.instantiatePersistent).toHaveBeenCalledWith({
-          key: "statePersistentKey",
-          storageKeys: ["test1", "test2"],
-        });
-        expect(statePersistent.initialLoading).not.toHaveBeenCalled();
-    */
+    expect(statePersistent.instantiatePersistent).toHaveBeenCalledWith({
+      key: undefined,
+      storageKeys: [],
+    });
+    expect(statePersistent.initialLoading).not.toHaveBeenCalled();
 
-    expect(statePersistent.key).toBe("statePersistentKey");
+    expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
     expect(statePersistent.ready).toBeTruthy();
     expect(statePersistent.isPersisted).toBeFalsy();
     expect(statePersistent.onLoad).toBeUndefined();
-    expect(statePersistent.storageKeys).toStrictEqual(["test1", "test2"]);
-    expect(statePersistent.defaultStorageKey).toBe("test1");
-
-    // expect(console.error).not.toHaveBeenCalled();
-
-    // Sleep 5ms because initialLoading happens async
-    return new Promise((resolve) => setTimeout(resolve, 5)).then(() => {
-      expect(dummyState.isPersisted).toBeFalsy(); // x
-    });
+    expect(statePersistent.storageKeys).toStrictEqual([]);
+    expect(statePersistent.defaultStorageKey).toBeUndefined();
   });
 
   describe("StatePersistent Function Tests", () => {
@@ -127,9 +125,9 @@ describe("StatePersistent Tests", () => {
         new Storage({
           key: "dummyStorage",
           methods: {
-            get: (key) => {},
-            remove: (key) => {},
-            set: (key, value) => {},
+            get: jest.fn(),
+            remove: jest.fn(),
+            set: jest.fn(),
           },
         })
       );
@@ -143,100 +141,75 @@ describe("StatePersistent Tests", () => {
         jest.spyOn(statePersistent, "validatePersistent");
       });
 
-      it("should update key with valid key in ready Persistent", () => {
+      it("should update key with valid key in ready Persistent", async () => {
         statePersistent.ready = true;
         statePersistent._key = "dummyKey";
 
-        return statePersistent.setKey("newKey").then(() => {
-          expect(statePersistent._key).toBe("newKey");
-          expect(statePersistent.ready).toBeTruthy();
-          expect(statePersistent.validatePersistent).toHaveBeenCalled();
-          expect(statePersistent.initialLoading).not.toHaveBeenCalled();
-          expect(statePersistent.persistValue).toHaveBeenCalledWith("newKey");
-          expect(statePersistent.removePersistedValue).toHaveBeenCalledWith(
-            "dummyKey"
-          );
-        });
+        await statePersistent.setKey("newKey");
+
+        expect(statePersistent._key).toBe("newKey");
+        expect(statePersistent.ready).toBeTruthy();
+        expect(statePersistent.validatePersistent).toHaveBeenCalled();
+        expect(statePersistent.initialLoading).not.toHaveBeenCalled();
+        expect(statePersistent.persistValue).toHaveBeenCalledWith("newKey");
+        expect(statePersistent.removePersistedValue).toHaveBeenCalledWith(
+          "dummyKey"
+        );
       });
 
-      it("should update key with not valid key in ready Persistent", () => {
+      it("should update key with not valid key in ready Persistent", async () => {
         statePersistent.ready = true;
         statePersistent._key = "dummyKey";
 
-        return statePersistent.setKey().then(() => {
-          expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
-          expect(statePersistent.ready).toBeFalsy();
-          expect(statePersistent.validatePersistent).toHaveBeenCalled();
-          expect(statePersistent.initialLoading).not.toHaveBeenCalled();
-          expect(statePersistent.persistValue).not.toHaveBeenCalled();
-          expect(statePersistent.removePersistedValue).toHaveBeenCalledWith(
-            "dummyKey"
-          );
-        });
+        await statePersistent.setKey();
+
+        expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
+        expect(statePersistent.ready).toBeFalsy();
+        expect(statePersistent.validatePersistent).toHaveBeenCalled();
+        expect(statePersistent.initialLoading).not.toHaveBeenCalled();
+        expect(statePersistent.persistValue).not.toHaveBeenCalled();
+        expect(statePersistent.removePersistedValue).toHaveBeenCalledWith(
+          "dummyKey"
+        );
       });
 
-      it("should update key with valid key in not ready Persistent", () => {
+      it("should update key with valid key in not ready Persistent", async () => {
         statePersistent.ready = false;
 
-        return statePersistent.setKey("newKey").then(() => {
-          expect(statePersistent._key).toBe("newKey");
-          expect(statePersistent.ready).toBeTruthy();
-          expect(statePersistent.validatePersistent).toHaveBeenCalled();
-          expect(statePersistent.initialLoading).toHaveBeenCalled();
-          expect(statePersistent.persistValue).not.toHaveBeenCalled();
-          expect(statePersistent.removePersistedValue).not.toHaveBeenCalled();
-        });
+        await statePersistent.setKey("newKey");
+
+        expect(statePersistent._key).toBe("newKey");
+        expect(statePersistent.ready).toBeTruthy();
+        expect(statePersistent.validatePersistent).toHaveBeenCalled();
+        expect(statePersistent.initialLoading).toHaveBeenCalled();
+        expect(statePersistent.persistValue).not.toHaveBeenCalled();
+        expect(statePersistent.removePersistedValue).not.toHaveBeenCalled();
       });
 
-      it("should update key with not valid key in not ready Persistent", () => {
+      it("should update key with not valid key in not ready Persistent", async () => {
         statePersistent.ready = false;
 
-        return statePersistent.setKey().then(() => {
-          expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
-          expect(statePersistent.ready).toBeFalsy();
-          expect(statePersistent.validatePersistent).toHaveBeenCalled();
-          expect(statePersistent.initialLoading).not.toHaveBeenCalled();
-          expect(statePersistent.persistValue).not.toHaveBeenCalled();
-          expect(statePersistent.removePersistedValue).not.toHaveBeenCalled();
-        });
+        await statePersistent.setKey();
+
+        expect(statePersistent._key).toBe(StatePersistent.placeHolderKey);
+        expect(statePersistent.ready).toBeFalsy();
+        expect(statePersistent.validatePersistent).toHaveBeenCalled();
+        expect(statePersistent.initialLoading).not.toHaveBeenCalled();
+        expect(statePersistent.persistValue).not.toHaveBeenCalled();
+        expect(statePersistent.removePersistedValue).not.toHaveBeenCalled();
       });
     });
 
-    // Note: Copied Persist initialLoading Tests, since idk how to mock super/extended class
     describe("initialLoading function tests", () => {
-      let onLoadSuccess = undefined;
-
       beforeEach(() => {
-        statePersistent.onLoad = (success) => {
-          onLoadSuccess = success;
-        };
-        jest.spyOn(statePersistent, "persistValue");
+        jest.spyOn(Persistent.prototype, "initialLoading");
       });
 
-      it("shouldn't call updateValue if value got loaded", () => {
-        statePersistent.loadPersistedValue = jest.fn(() =>
-          Promise.resolve(true)
-        );
+      it("shouldn't call updateValue if value got loaded", async () => {
+        await statePersistent.initialLoading();
 
-        statePersistent.initialLoading().then(() => {
-          expect(statePersistent.loadPersistedValue).toHaveBeenCalled();
-          expect(statePersistent.persistValue).not.toHaveBeenCalled();
-          expect(onLoadSuccess).toBeTruthy();
-          expect(dummyState.isPersisted).toBeTruthy();
-        });
-      });
-
-      it("should call updateValue if value doesn't got loaded", () => {
-        statePersistent.loadPersistedValue = jest.fn(() =>
-          Promise.resolve(false)
-        );
-
-        statePersistent.initialLoading().then(() => {
-          expect(statePersistent.loadPersistedValue).toHaveBeenCalled();
-          expect(statePersistent.persistValue).toHaveBeenCalled();
-          expect(onLoadSuccess).toBeFalsy();
-          expect(dummyState.isPersisted).toBeTruthy();
-        });
+        expect(Persistent.prototype.initialLoading).toHaveBeenCalled();
+        expect(dummyState.isPersisted).toBeTruthy();
       });
     });
 
@@ -303,7 +276,7 @@ describe("StatePersistent Tests", () => {
         expect(statePersistent.persistValue).toHaveBeenCalledWith("coolKey");
       });
 
-      it("shouldn't load value if persistent isn't ready", async () => {
+      it("shouldn't load value if Persistent isn't ready", async () => {
         statePersistent.ready = false;
         dummyAgile.storages.get = jest.fn(() =>
           Promise.resolve(undefined as any)
@@ -359,7 +332,7 @@ describe("StatePersistent Tests", () => {
         expect(statePersistent.isPersisted).toBeTruthy();
       });
 
-      it("shouldn't persist Value if persistent isn't ready", async () => {
+      it("shouldn't persist Value if Persistent isn't ready", async () => {
         statePersistent.ready = false;
 
         const response = await statePersistent.persistValue();
@@ -410,7 +383,7 @@ describe("StatePersistent Tests", () => {
         expect(statePersistent.isPersisted).toBeFalsy();
       });
 
-      it("shouldn't remove persistedValue if persistent isn't ready", async () => {
+      it("shouldn't remove persistedValue if Persistent isn't ready", async () => {
         statePersistent.ready = false;
 
         const response = await statePersistent.removePersistedValue("coolKey");
@@ -423,7 +396,7 @@ describe("StatePersistent Tests", () => {
     });
 
     describe("formatKey function tests", () => {
-      it("should return the key of the state if not key got passed", () => {
+      it("should return key of State if no key got passed", () => {
         dummyState._key = "coolKey";
 
         const response = statePersistent.formatKey();
@@ -439,7 +412,7 @@ describe("StatePersistent Tests", () => {
         expect(response).toBe("awesomeKey");
       });
 
-      it("should return and apply passed key to state if state has no key yet", () => {
+      it("should return and apply passed key to State if state has no key yet", () => {
         dummyState._key = undefined;
 
         const response = statePersistent.formatKey("awesomeKey");
@@ -448,7 +421,7 @@ describe("StatePersistent Tests", () => {
         expect(dummyState._key).toBe("awesomeKey");
       });
 
-      it("should return undefined if no key got passed and the state has no key", () => {
+      it("should return undefined if no key got passed and State has no key", () => {
         dummyState._key = undefined;
 
         const response = statePersistent.formatKey();
@@ -462,7 +435,7 @@ describe("StatePersistent Tests", () => {
         dummyAgile.storages.set = jest.fn();
       });
 
-      it("should save State value in Storage (default config)", () => {
+      it("should save State Value in Storage (default config)", () => {
         statePersistent.rebuildStorageSideEffect(dummyState, "coolKey");
 
         expect(dummyAgile.storages.set).toHaveBeenCalledWith(
@@ -472,7 +445,7 @@ describe("StatePersistent Tests", () => {
         );
       });
 
-      it("shouldn't save State value in Storage (config.storage = false)", () => {
+      it("shouldn't save State Value in Storage (config.storage = false)", () => {
         statePersistent.rebuildStorageSideEffect(dummyState, "coolKey", {
           storage: false,
         });
