58812c8b4bbec07566dfec923bddd878bd24497f
Benno Kohrs <bennokohrs@gmail.com>
2020-12-18 11:12:51 +0100

    refactored integration tests

--- packages/core/tests/new/integrations/integration.test.ts
+++ packages/core/tests/new/integrations/integration.test.ts
@@ -14,10 +14,34 @@ describe("Integration Tests", () => {
 
     const integration = new Integration(integrationConfig);
 
-    expect(integration.ready).toBeFalsy();
-    expect(integration.integrated).toBeFalsy();
     expect(integration._key).toBe("test");
     expect(integration.frameworkInstance).toStrictEqual({ react: "native" });
+    expect(integration.ready).toBeFalsy();
+    expect(integration.integrated).toBeFalsy();
     expect(integration.methods).toStrictEqual(methods);
+  });
+
+  describe("Integration Function Tests", () => {
+    let integration: Integration;
+
+    beforeEach(() => {
+      integration = new Integration({ key: "dummyIntegration" });
+    });
+
+    describe("key set function tests", () => {
+      it("should update key in Integration", () => {
+        integration.key = "myCoolKey";
+
+        expect(integration._key).toBe("myCoolKey");
+      });
+    });
+
+    describe("key get function tests", () => {
+      it("should return current key of Integration", () => {
+        integration._key = "myCoolKey";
+
+        expect(integration.key).toBe("myCoolKey");
+      });
+    });
   });
 });
