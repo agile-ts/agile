d11ee672a1ece98c750dd8b3deebdce7e689ddb7
Benno Kohrs <bennokohrs@gmail.com>
2020-12-18 09:16:47 +0100

    refactored observer tests

--- packages/core/tests/new/runtime/observer.test.ts
+++ packages/core/tests/new/runtime/observer.test.ts
@@ -15,6 +15,7 @@ describe("Observer Tests", () => {
     dummySubscription2 = new SubscriptionContainer();
 
     console.warn = jest.fn();
+    jest.spyOn(Observer.prototype, "subscribe");
   });
 
   it("should create Observer (default config)", () => {
@@ -42,6 +43,9 @@ describe("Observer Tests", () => {
     expect(observer.subs.size).toBe(2);
     expect(observer.subs.has(dummySubscription1)).toBeTruthy();
     expect(observer.subs.has(dummySubscription2)).toBeTruthy();
+
+    expect(observer.subscribe).toHaveBeenCalledWith(dummySubscription1);
+    expect(observer.subscribe).toHaveBeenCalledWith(dummySubscription2);
   });
 
   describe("Observer Function Tests", () => {
@@ -49,6 +53,34 @@ describe("Observer Tests", () => {
 
     beforeEach(() => {
       observer = new Observer(dummyAgile, { key: "observer" });
+    });
+
+    describe("key set function tests", () => {
+      it("should update key in Observer", () => {
+        observer.key = "myNewDummyKey";
+
+        expect(observer._key).toBe("myNewDummyKey");
+      });
+    });
+
+    describe("key get function tests", () => {
+      it("should return current key of Observer", () => {
+        observer._key = "myDummyKey";
+
+        expect(observer.key).toBe("myDummyKey");
+      });
+    });
+
+    describe("perform function tests", () => {
+      it("should print warning", () => {
+        const dummyJob = new Job(observer);
+
+        observer.perform(dummyJob);
+
+        expect(console.warn).toHaveBeenCalledWith(
+          "Agile Warn: Perform function isn't Set in Observer! Be aware that Observer is no stand alone class!"
+        );
+      });
     });
 
     describe("depend function tests", () => {
@@ -60,17 +92,16 @@ describe("Observer Tests", () => {
         dummyObserver2 = new Observer(dummyAgile, { key: "dummyObserver2" });
       });
 
-      it("should add observer to deps", () => {
+      it("should add passed Observer to deps", () => {
         observer.depend(dummyObserver1);
-        observer.depend(dummyObserver2);
 
-        expect(observer.deps.size).toBe(2);
-        expect(observer.deps.has(dummyObserver1));
+        expect(observer.deps.size).toBe(1);
         expect(observer.deps.has(dummyObserver2));
       });
 
-      it("shouldn't add same observer twice to deps", () => {
+      it("shouldn't add the same Observer twice to deps", () => {
         observer.depend(dummyObserver1);
+
         observer.depend(dummyObserver1);
 
         expect(observer.deps.size).toBe(1);
@@ -87,27 +118,22 @@ describe("Observer Tests", () => {
         dummySubscriptionContainer2 = new SubscriptionContainer();
       });
 
-      it("should add subscriptionContainer to subs", () => {
-        observer.subscribe(dummySubscriptionContainer1);
-        observer.subscribe(dummySubscriptionContainer2);
-
-        expect(observer.subs.size).toBe(2);
-        expect(observer.subs.has(dummySubscriptionContainer1));
-        expect(observer.subs.has(dummySubscriptionContainer2));
-
-        expect(dummySubscriptionContainer1.subs.size).toBe(1);
-        expect(dummySubscriptionContainer1.subs.has(observer)).toBeTruthy();
-        expect(dummySubscriptionContainer2.subs.size).toBe(1);
-        expect(dummySubscriptionContainer2.subs.has(observer)).toBeTruthy();
-      });
-
-      it("shouldn't add same subscriptionContainer twice to subs", () => {
-        observer.subscribe(dummySubscriptionContainer1);
+      it("should add subscriptionContainer to subs and this(Observer) to SubscriptionContainer subs", () => {
         observer.subscribe(dummySubscriptionContainer1);
 
         expect(observer.subs.size).toBe(1);
         expect(observer.subs.has(dummySubscriptionContainer1));
+        expect(dummySubscriptionContainer1.subs.size).toBe(1);
+        expect(dummySubscriptionContainer1.subs.has(observer)).toBeTruthy();
+      });
 
+      it("shouldn't add same subscriptionContainer twice to subs", () => {
+        observer.subscribe(dummySubscriptionContainer1);
+
+        observer.subscribe(dummySubscriptionContainer1);
+
+        expect(observer.subs.size).toBe(1);
+        expect(observer.subs.has(dummySubscriptionContainer1));
         expect(dummySubscriptionContainer1.subs.size).toBe(1);
         expect(dummySubscriptionContainer1.subs.has(observer)).toBeTruthy();
       });
@@ -124,29 +150,14 @@ describe("Observer Tests", () => {
         observer.subscribe(dummySubscriptionContainer2);
       });
 
-      it("should remove subscriptionContainer from subs", () => {
+      it("should remove subscriptionContainer from subs and this(Observer) from SubscriptionContainer subs", () => {
         observer.unsubscribe(dummySubscriptionContainer1);
 
         expect(observer.subs.size).toBe(1);
         expect(observer.subs.has(dummySubscriptionContainer1));
-
         expect(dummySubscriptionContainer1.subs.size).toBe(0);
         expect(dummySubscriptionContainer2.subs.size).toBe(1);
         expect(dummySubscriptionContainer2.subs.has(observer)).toBeTruthy();
-      });
-    });
-
-    describe("functions that get overwritten tests | because Observer is no stand alone class", () => {
-      describe("perform function tests", () => {
-        it("should print warning", () => {
-          const dummyJob = new Job(observer);
-
-          observer.perform(dummyJob);
-
-          expect(console.warn).toHaveBeenCalledWith(
-            "Agile Warn: Perform function isn't Set in Observer! Be aware that Observer is no stand alone class!"
-          );
-        });
       });
     });
   });
