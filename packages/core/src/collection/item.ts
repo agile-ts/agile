f418e015c7834fe88032b18611610d54852fd518
Benno Kohrs <bennokohrs@gmail.com>
2020-12-18 10:30:54 +0100

    optimized updating key of item

--- packages/core/src/collection/item.ts
+++ packages/core/src/collection/item.ts
@@ -1,7 +1,7 @@
 import { State, Collection, DefaultItem, StateKey } from "../internal";
 
 export class Item<DataType = DefaultItem> extends State<DataType> {
-  private collection: () => Collection<DataType>;
+  public collection: () => Collection<DataType>;
 
   /**
    * @public
@@ -14,33 +14,27 @@ export class Item<DataType = DefaultItem> extends State<DataType> {
     this.collection = () => collection;
 
     // Setting primaryKey of Data to Key/Name of Item
-    this.key = data[collection.config.primaryKey];
+    this.setKey(data[collection.config.primaryKey]);
   }
 
+  //=========================================================================================================
+  // Set Key
+  //=========================================================================================================
   /**
-   * @public
-   * Set Key/Name of Item
+   * @internal
+   * Updates Key/Name of State
+   * @param value - New Key/Name of State
    */
-  public set key(value: StateKey | undefined) {
-    // Note can't use 'super.key' because of 'https://github.com/Microsoft/TypeScript/issues/338'
-    this.setKey(value);
-    if (!value) return;
+  public setKey(value: StateKey | undefined): this {
+    super.setKey(value);
+
+    if (!value) return this;
 
     // Update rebuildGroupThatIncludePrimaryKey SideEffect
     this.removeSideEffect("rebuildGroup");
     this.addSideEffect("rebuildGroup", (properties: any) =>
       this.collection().rebuildGroupsThatIncludeItemKey(value, properties)
     );
-  }
-
-  /**
-   * @public
-   * Get Key/Name of Item
-   */
-  public get key(): StateKey | undefined {
-    // Note can't use 'super.key' because of 'https://github.com/Microsoft/TypeScript/issues/338'
-    // Can't remove this getter function.. because the setter function is set in this class -> Error if not setter and getter function set
-
-    return this._key;
+    return this;
   }
 }
