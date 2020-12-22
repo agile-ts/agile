4df2833f5be5951b715ed5f2f44f16fe29b50510
Benno Kohrs <bennokohrs@gmail.com>
2020-12-22 07:54:15 +0100

    created initSelectors and initGroups function tests

--- packages/core/tests/new/collection/collection.test.ts
+++ packages/core/tests/new/collection/collection.test.ts
@@ -189,5 +189,144 @@ describe("Collection Tests", () => {
         expect(collection.persistent.setKey).not.toHaveBeenCalled();
       });
     });
+
+    describe("Group function tests", () => {
+      it("should create Group which belongs to Collection", () => {
+        const response = collection.Group([1, 2], {
+          key: "group1Key",
+        });
+
+        expect(response).toBeInstanceOf(Group);
+        expect(response._key).toBe("group1Key");
+        expect(response._value).toStrictEqual([1, 2]);
+        expect(response.collection()).toBe(collection);
+      });
+    });
+
+    describe("Selector function tests", () => {
+      it("should create Selector which belongs to Collection", () => {
+        const response = collection.Selector("id1", {
+          key: "selector1Key",
+        });
+
+        expect(response).toBeInstanceOf(Selector);
+        expect(response._key).toBe("selector1Key");
+        expect(response._itemKey).toBe("id1");
+        expect(response.collection()).toBe(collection);
+      });
+    });
+
+    describe("initGroups function tests", () => {
+      it("should create GroupsObject out of passed GroupKeys Array and add defaultGroup", () => {
+        collection.initGroups(["group1", "group2"]);
+
+        expect(collection.groups).toHaveProperty("group1");
+        expect(collection.groups["group1"]._key).toBe("group1");
+        expect(collection.groups["group1"]._value).toStrictEqual([]);
+        expect(collection.groups["group1"].collection()).toBe(collection);
+
+        expect(collection.groups).toHaveProperty("group2");
+        expect(collection.groups["group2"]._key).toBe("group2");
+        expect(collection.groups["group2"]._value).toStrictEqual([]);
+        expect(collection.groups["group2"].collection()).toBe(collection);
+
+        expect(collection.groups).toHaveProperty(
+          collection.config.defaultGroupKey as any
+        );
+        expect(collection.groups[collection.config.defaultGroupKey]._key).toBe(
+          "default"
+        );
+        expect(
+          collection.groups[collection.config.defaultGroupKey]._value
+        ).toStrictEqual([]);
+        expect(
+          collection.groups[collection.config.defaultGroupKey].collection()
+        ).toBe(collection);
+      });
+
+      it("should create GroupsObject out of passed Groups Object and add default Group", () => {
+        let dummyGroup1 = new Group(collection);
+        let dummyGroup2 = new Group(collection, ["test1", "test2"], {
+          key: "overwrittenKey",
+        });
+
+        collection.initGroups({
+          dummyGroup1: dummyGroup1,
+          dummyGroup2: dummyGroup2,
+        });
+
+        expect(collection.groups).toHaveProperty("dummyGroup1");
+        expect(collection.groups["dummyGroup1"]._key).toBe("dummyGroup1");
+        expect(collection.groups["dummyGroup1"]._value).toStrictEqual([]);
+        expect(collection.groups["dummyGroup1"].collection()).toBe(collection);
+
+        expect(collection.groups).toHaveProperty("dummyGroup2");
+        expect(collection.groups["dummyGroup2"]._key).toBe("overwrittenKey");
+        expect(collection.groups["dummyGroup2"]._value).toStrictEqual([
+          "test1",
+          "test2",
+        ]);
+        expect(collection.groups["dummyGroup2"].collection()).toBe(collection);
+
+        expect(collection.groups).toHaveProperty(
+          collection.config.defaultGroupKey as any
+        );
+        expect(collection.groups[collection.config.defaultGroupKey]._key).toBe(
+          "default"
+        );
+        expect(
+          collection.groups[collection.config.defaultGroupKey]._value
+        ).toStrictEqual([]);
+        expect(
+          collection.groups[collection.config.defaultGroupKey].collection()
+        ).toBe(collection);
+      });
+    });
+
+    describe("initSelectors function tests", () => {
+      it("should create SelectorsObject out of passed SelectorKeys Array", () => {
+        collection.initSelectors(["selector1", "selector2"]);
+
+        expect(collection.selectors).toHaveProperty("selector1");
+        expect(collection.selectors["selector1"]._key).toBe("selector1");
+        expect(collection.selectors["selector1"]._itemKey).toBe("selector1");
+        expect(collection.selectors["selector1"].collection()).toBe(collection);
+
+        expect(collection.selectors).toHaveProperty("selector2");
+        expect(collection.selectors["selector2"]._key).toBe("selector2");
+        expect(collection.selectors["selector2"]._itemKey).toBe("selector2");
+        expect(collection.selectors["selector2"].collection()).toBe(collection);
+      });
+
+      it("should create SelectorsObject out of passed Selector Object", () => {
+        let dummySelector1 = new Selector(collection, "1");
+        let dummySelector2 = new Selector(collection, "2", {
+          key: "overwrittenKey",
+        });
+
+        collection.initSelectors({
+          dummySelector1: dummySelector1,
+          dummySelector2: dummySelector2,
+        });
+
+        expect(collection.selectors).toHaveProperty("dummySelector1");
+        expect(collection.selectors["dummySelector1"]._key).toBe(
+          "dummySelector1"
+        );
+        expect(collection.selectors["dummySelector1"]._itemKey).toBe("1");
+        expect(collection.selectors["dummySelector1"].collection()).toBe(
+          collection
+        );
+
+        expect(collection.selectors).toHaveProperty("dummySelector2");
+        expect(collection.selectors["dummySelector2"]._key).toBe(
+          "overwrittenKey"
+        );
+        expect(collection.selectors["dummySelector2"]._itemKey).toBe("2");
+        expect(collection.selectors["dummySelector2"].collection()).toBe(
+          collection
+        );
+      });
+    });
   });
 });
