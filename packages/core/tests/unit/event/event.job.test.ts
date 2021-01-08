76f9ecac9ac74fee8f4ebaf0066d9bc7afa2f441
Benno Kohrs <bennokohrs@gmail.com>
2021-01-08 07:46:23 +0100

    fixed #60

--- packages/core/tests/unit/event/event.job.test.ts
+++ packages/core/tests/unit/event/event.job.test.ts
@@ -5,7 +5,10 @@ describe('EventJob Tests', () => {
     const eventJob = new EventJob('myPayload');
 
     expect(eventJob.payload).toBe('myPayload');
-    expect(eventJob.creationTimestamp).toBeCloseTo(new Date().getTime(), -1);
+    expect(eventJob.creationTimestamp).toBeCloseTo(
+      new Date().getTime(),
+      calculatePrecision(100)
+    );
     expect(eventJob.keys).toBeUndefined();
   });
 
@@ -13,7 +16,15 @@ describe('EventJob Tests', () => {
     const eventJob = new EventJob('myPayload', ['dummyKey1', 'dummyKey2']);
 
     expect(eventJob.payload).toBe('myPayload');
-    expect(eventJob.creationTimestamp).toBeCloseTo(new Date().getTime(), -1);
+    expect(eventJob.creationTimestamp).toBeCloseTo(
+      new Date().getTime(),
+      calculatePrecision(100)
+    );
     expect(eventJob.keys).toStrictEqual(['dummyKey1', 'dummyKey2']);
   });
 });
+
+// https://github.com/facebook/jest/issues/5218
+function calculatePrecision(deviation) {
+  return -Math.log10(2 * deviation);
+}
