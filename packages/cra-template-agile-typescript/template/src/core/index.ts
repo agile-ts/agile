5f1876033f73b87ab16b1b474c838feaddda4842
Benno Kohrs <bennokohrs@gmail.com>
2021-01-23 18:34:10 +0100

    fixed templates

--- packages/cra-template-agile-typescript/template/src/core/index.ts
+++ packages/cra-template-agile-typescript/template/src/core/index.ts
@@ -1,8 +1,9 @@
 import { Agile, Logger } from '@agile-ts/core';
+import reactIntegration from '@agile-ts/react';
 
 export const App = new Agile({
   logConfig: { level: Logger.level.DEBUG, timestamp: true },
-});
+}).integrate(reactIntegration);
 
 export const MY_STATE = App.createState<string>('MyState');
 export const MY_STATE_2 = App.createState<string>('MyState2', {
