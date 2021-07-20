c038a69e7b6813bcd25ec89101ec8f7b3a6167bb
Benno Kohrs <bennokohrs@gmail.com>
2021-07-20 08:24:21 +0200

    fixed lint error in template

--- packages/cra-template-agile/template/src/pages/Home/index.jsx
+++ packages/cra-template-agile/template/src/pages/Home/index.jsx
@@ -3,46 +3,40 @@ import Stopwatch from '../../components/Stopwatch';
 
 import styles from './Home.module.css';
 
-function Home() {
-  return (
-    <div className={styles.container}>
-      {/* Header */}
-      <h1 className={styles.title}>
-        Welcome to <a href="https://agile-ts.org">AgileTs!</a>
-      </h1>
-      <h3 className={styles.subtitle}>
-        in a <a href="https://reactjs.org">React</a> application
-      </h3>
+const Home = () => (
+  <div className={styles.container}>
+    {/* Header */}
+    <h1 className={styles.title}>
+      Welcome to <a href="https://agile-ts.org">AgileTs!</a>
+    </h1>
+    <h3 className={styles.subtitle}>
+      in a <a href="https://reactjs.org">React</a> application
+    </h3>
 
-      <div className={styles.divider} />
+    <div className={styles.divider} />
 
-      {/* Stopwatch */}
-      <p className={styles.getStartedText}>
-        Get started by editing{' '}
-        <code className={styles.code}>core/index.js</code>
-      </p>
-      <Stopwatch />
+    {/* Stopwatch */}
+    <p className={styles.getStartedText}>
+      Get started by editing <code className={styles.code}>core/index.ts</code>
+    </p>
+    <Stopwatch />
 
-      {/* Cards */}
-      <div className={styles.cardsContainer}>
-        <a
-          href="https://agile-ts.org/docs/introduction/"
-          className={styles.card}
-        >
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about AgileTs features and API.</p>
-        </a>
+    {/* Cards */}
+    <div className={styles.cardsContainer}>
+      <a href="https://agile-ts.org/docs/introduction/" className={styles.card}>
+        <h2>Documentation &rarr;</h2>
+        <p>Find in-depth information about AgileTs features and API.</p>
+      </a>
 
-        <a
-          href="https://github.com/agile-ts/agile/discussions"
-          className={styles.card}
-        >
-          <h2>Help &rarr;</h2>
-          <p>Ask your questions and get help by the community.</p>
-        </a>
-      </div>
+      <a
+        href="https://github.com/agile-ts/agile/discussions"
+        className={styles.card}
+      >
+        <h2>Help &rarr;</h2>
+        <p>Ask your questions and get help by the community.</p>
+      </a>
     </div>
-  );
-}
+  </div>
+);
 
 export default Home;
