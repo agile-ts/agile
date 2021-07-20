525bcdfb99ac12d40ffd9af8c71ce29ed3b1b923
Benno Kohrs <bennokohrs@gmail.com>
2021-07-20 19:43:56 +0200

    added stop watch persist query url example

---
+++ examples/react/release/stopwatch-query-url/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/Home';
+
+import './global.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
