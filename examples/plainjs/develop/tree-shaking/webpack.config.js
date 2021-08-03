32f4bdbdffb4f7f7e1ad34181cbf2b9d30cfe4db
Benno Kohrs <bennokohrs@gmail.com>
2021-08-03 07:55:03 +0200

    fixed typo

--- examples/plainjs/develop/tree-shaking/webpack.config.js
+++ examples/plainjs/develop/tree-shaking/webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    mode: "development",
-    optimization: {
-        usedExports: true,
-        innerGraph: true,
-        sideEffects: true,
-    },
-    devtool: false,
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  optimization: {
+    usedExports: true,
+    innerGraph: true,
+    sideEffects: true,
+  },
+  devtool: false,
 };
