809efa4f833889394ed89987658f5d20961a878f
Benno Kohrs <bennokohrs@gmail.com>
2021-08-07 21:31:37 +0200

    implemented yargs

--- benchmark/runtime/run.ts
+++ benchmark/runtime/run.ts
@@ -1,22 +1,55 @@
 import dotenv from 'dotenv';
-import { startSpeedBench } from './benchmarkTypes';
+import { startBundleBench, startSpeedBench } from './benchmarkTypes';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 // Loads environment variables from the '.env' file
 dotenv.config();
 
 // TODO implement yargs https://yargs.js.org/
 
-// https://nodejs.org/docs/latest/api/process.html#process_process_argv
-// Extract entry (at third parameter) from the executed command
-// yarn run ./path/to/entry -> './path/to/entry' is extracted
-const entry = process.argv.slice(2)[0];
-const isDev =
-  process.argv.slice(2)[1] === '--dev' || process.env.DEV === 'true';
+// hideBind handles the 'process.argv.slice' logic
+const argv = yargs(hideBin(process.argv))
+  .option('_', {
+    type: 'string',
+    default: ['./benchmarks/react/counter'],
+    description: 'What benchmark to execute',
+  })
+  .option('dev', {
+    type: 'boolean',
+    default: false,
+    description:
+      'Whether to start the benchmark/s in developer mode for better debugging.',
+  })
+  .option('type', {
+    type: 'string',
+    default: 'speed',
+    description: 'What type of benchmark to be executed',
+  }).argv;
+
+console.log(argv);
+
+const entry = argv._[0];
+const isDev = argv.dev;
+const benchmarkType = argv.type;
+
 if (entry == null) {
   throw new Error(
     "No valid entry was provided! Valid entry example: 'yarn run ./benchmarks/react/counter'"
   );
 }
 
-// Execute the Benchmark
-startSpeedBench(entry, isDev);
+// Benchmarks that can be executed marked with a unique identifier
+const benchmarks: {
+  [key: string]: (entry: string, isDev: boolean) => Promise<void>;
+} = {
+  speed: startSpeedBench,
+  bundle: startBundleBench,
+};
+
+// Execute Benchmark based on the specified Benchmark type
+const toExecuteBenchmark = benchmarks[benchmarkType];
+if (toExecuteBenchmark != null) toExecuteBenchmark(entry, isDev);
+else {
+  benchmarks['speed'](entry, isDev);
+}
