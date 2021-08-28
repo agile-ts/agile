5ea27e28dabdda9f48ad31cb160a22b10dc5cad9
BennoDev <bennokohrs@gmail.com>
2021-08-28 10:05:18 +0200

    fixed typos

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,20 +1,17 @@
-//=========================================================================================================
-// Copy
-//=========================================================================================================
 /**
- * @internal
- * Creates a fresh copy of an Array/Object
+ * Creates a fresh (deep) copy of the specified value.
  * https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/
- * @param value - Array/Object that gets copied
+ *
+ * @public
+ * @param value - Value to be copied.
  */
 export function copy<T = any>(value: T): T {
   // Extra checking 'value == null' because 'typeof null === object'
   if (value == null || typeof value !== 'object') return value;
 
   // Ignore everything that is no object or array but has the type of an object (e.g. classes)
-  const valConstructorName = Object.getPrototypeOf(
-    value
-  ).constructor.name.toLowerCase();
+  const valConstructorName =
+    Object.getPrototypeOf(value).constructor.name.toLowerCase();
   if (valConstructorName !== 'object' && valConstructorName !== 'array')
     return value;
 
@@ -27,15 +24,13 @@ export function copy<T = any>(value: T): T {
   return newObject as T;
 }
 
-//=========================================================================================================
-// Is Valid Object
-//=========================================================================================================
 /**
- * @internal
- * Checks if passed value is a valid Object
+ * Checks whether the specified value is a valid object.
  * https://stackoverflow.com/questions/12996871/why-does-typeof-array-with-objects-return-object-and-not-array
- * @param value - Value that is tested for its correctness
- * @param considerArray - Whether Arrays should be considered as object
+ *
+ * @public
+ * @param value - Value
+ * @param considerArray - Whether to considered an array as an object.
  */
 export function isValidObject(value: any, considerArray = false): boolean {
   function isHTMLElement(obj: any) {
@@ -59,12 +54,10 @@ export function isValidObject(value: any, considerArray = false): boolean {
   );
 }
 
-//=========================================================================================================
-// Includes Array
-//=========================================================================================================
 /**
- * @internal
- * Check if array1 contains all elements of array2
+ * Checks whether 'array1' contains all elements of 'array2'.
+ *
+ * @public
  * @param array1 - Array 1
  * @param array2 - Array 2
  */
@@ -75,13 +68,11 @@ export function includesArray<DataType = any>(
   return array2.every((element) => array1.includes(element));
 }
 
-//=========================================================================================================
-// Normalize Array
-//=========================================================================================================
 /**
- * @internal
- * Transforms Item/s to an Item Array
- * @param items - Item/s that gets transformed to an Array
+ * Transforms Item/s into an array of Items.
+ *
+ * @public
+ * @param items - Item/s to be transformed into an array of Items.
  * @param config - Config
  */
 export function normalizeArray<DataType = any>(
@@ -95,25 +86,21 @@ export function normalizeArray<DataType = any>(
   return Array.isArray(items) ? items : [items as DataType];
 }
 
-//=========================================================================================================
-// Is Function
-//=========================================================================================================
 /**
- * @internal
- * Checks if value is a function
- * @param value - Value that gets tested if its a function
+ * Checks whether the specified function is a function.
+ *
+ * @public
+ * @param value - Value to be checked
  */
 export function isFunction(value: any): boolean {
   return typeof value === 'function';
 }
 
-//=========================================================================================================
-// Is Async Function
-//=========================================================================================================
 /**
- * @internal
- * Checks if value is an async function
- * @param value - Value that gets tested if its an async function
+ * Checks whether the specified function is an async function.
+ *
+ * @public
+ * @param value - Value to be checked.
  */
 export function isAsyncFunction(value: any): boolean {
   const valueString = value.toString();
@@ -124,13 +111,11 @@ export function isAsyncFunction(value: any): boolean {
   );
 }
 
-//=========================================================================================================
-// Is Json String
-//=========================================================================================================
 /**
- * @internal
- * Checks if value is valid JsonString
- * @param value - Value that gets checked
+ * Checks whether the specified value is a valid JSON string
+ *
+ * @public
+ * @param value - Value to be checked.
  */
 export function isJsonString(value: any): boolean {
   if (typeof value !== 'string') return false;
@@ -142,15 +127,13 @@ export function isJsonString(value: any): boolean {
   return true;
 }
 
-//=========================================================================================================
-// Define Config
-//=========================================================================================================
 /**
- * @internal
- * Merges default values/properties into config object
- * @param config - Config object that receives default values
- * @param defaults - Default values object that gets merged into config object
- * @param overwriteUndefinedProperties - If undefined Properties in config gets overwritten by the default value
+ * Merges the default values object ('defaults') into the configuration object ('config').
+ *
+ * @public
+ * @param config - Configuration object to merge the default values in.
+ * @param defaults - Default values object to be merged into the configuration object.
+ * @param overwriteUndefinedProperties - Whether to overwrite 'undefined' set properties with default values.
  */
 export function defineConfig<ConfigInterface = Object>(
   config: ConfigInterface,
@@ -174,24 +157,22 @@ export function defineConfig<ConfigInterface = Object>(
   return shallowCopiedConfig;
 }
 
-//=========================================================================================================
-// Flat Merge
-//=========================================================================================================
-/**
- * @internal
- * @param addNewProperties - Adds new properties to source Object
- */
 export interface FlatMergeConfigInterface {
+  /**
+   *
+   * Whether to add new properties (properties that doesn't exist in the source object yet) to the source object.
+   * @default true
+   */
   addNewProperties?: boolean;
 }
 
 /**
- * @internal
- * Merges items into object, be aware that the merge will only happen at the top level of the object.
- * Initially it adds new properties of the changes object into the source object.
+ * Merges the 'changes' object into the 'source' object at top level.
+ *
+ * @public
  * @param source - Source object
- * @param changes - Changes that get merged into the source object
- * @param config - Config
+ * @param changes - Changes object to be merged into the source object
+ * @param config - Configuration object
  */
 export function flatMerge<DataType = Object>(
   source: DataType,
@@ -219,14 +200,12 @@ export function flatMerge<DataType = Object>(
   return _source;
 }
 
-//=========================================================================================================
-// Equals
-//=========================================================================================================
 /**
- * @internal
- * Check if two values are equal
- * @param value1 - First Value
- * @param value2 - Second Value
+ * Checks whether the two specified values are equivalent.
+ *
+ * @public
+ * @param value1 - First value.
+ * @param value2 - Second value.
  */
 export function equal(value1: any, value2: any): boolean {
   return (
@@ -239,46 +218,44 @@ export function equal(value1: any, value2: any): boolean {
   );
 }
 
-//=========================================================================================================
-// Not Equals
-//=========================================================================================================
 /**
- * @internal
- * Checks if two values aren't equal
- * @param value1 - First Value
- * @param value2 - Second Value
+ * Checks whether the two specified values are NOT equivalent.
+ *
+ * @public
+ * @param value1 - First value.
+ * @param value2 - Second value.
  */
 export function notEqual(value1: any, value2: any): boolean {
   return !equal(value1, value2);
 }
 
-//=========================================================================================================
-// Generate Id
-//=========================================================================================================
 /**
- * @internal
- * Generates random Id
- * @param length - Length of generated Id
+ * Generates a randomized id based on alphabetic and numeric characters.
+ *
+ * @public
+ * @param length - Length of the to generate id (default = 5).
+ * @param characters - Characters to generate the id from (default = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789').
  */
-export function generateId(length?: number): string {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+export function generateId(
+  length: number = 5,
+  characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+): string {
   const charactersLength = characters.length;
   let result = '';
-  if (!length) length = 5;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
 
-//=========================================================================================================
-// Create Array From Object
-//=========================================================================================================
 /**
- * @internal
- * Transforms Object to Array
- * @param object - Object that gets transformed
+ * Transforms the specified object into an array.
+ *
+ * Example:
+ * {"1": 'jeff', 2: 'frank'} -> [{key: "1", instance: 'jeff'}, {key: 2, instance: 'frank'}]
+ *
+ * @public
+ * @param object - Object to be transformed to an array.
  */
 export function createArrayFromObject<P = any>(object: {
   [key: string]: P;
@@ -293,13 +270,11 @@ export function createArrayFromObject<P = any>(object: {
   return array;
 }
 
-//=========================================================================================================
-// Clone
-//=========================================================================================================
 /**
- * @internal
- * Clones a Class
- * @param instance - Instance of Class you want to clone
+ * Clones the specified class.
+ *
+ * @public
+ * @param instance - Class to be cloned.
  */
 export function clone<T = any>(instance: T): T {
   // Clone Class
@@ -312,14 +287,12 @@ export function clone<T = any>(instance: T): T {
   return objectClone;
 }
 
-//=========================================================================================================
-// Remove Properties
-//=========================================================================================================
 /**
- * @internal
- * Removes properties from Object
- * @param object - Object from which the properties get removed
- * @param properties - Properties that get removed from the object
+ * Removes specified properties from the defined object.
+ *
+ * @public
+ * @param object - Object to remove the specified properties from.
+ * @param properties - Property keys to be removed from the specified object.
  */
 export function removeProperties<T = Object>(
   object: T,
